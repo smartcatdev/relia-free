05f01e4020a2a9e9bc48bb230786ebf53b44d423
smartcatdev <bilal@smartcat.ca>
2018-12-17 11:46:35 -0500

    woocommerce updates

--- woocommerce/content-product.php
+++ woocommerce/content-product.php
@@ -13,7 +13,7 @@
  * @see     https://docs.woocommerce.com/document/template-structure/
  * @author  WooThemes
  * @package WooCommerce/Templates
- * @version 3.0.0
+ * @version 3.4.0
  */
 
 if ( ! defined( 'ABSPATH' ) ) {
@@ -27,7 +27,7 @@ if ( empty( $product ) || ! $product->is_visible() ) {
 	return;
 }
 ?>
-<li <?php post_class(); ?>>
+<li <?php wc_product_class(); ?>>
 	<?php
 	/**
 	 * woocommerce_before_shop_loop_item hook.
@@ -45,8 +45,8 @@ if ( empty( $product ) || ! $product->is_visible() ) {
 	do_action( 'woocommerce_before_shop_loop_item_title' ); ?>
 
         <div class="product-content">
-    
-            <?php 
+
+            <?php
             /**
              * woocommerce_shop_loop_item_title hook.
              *
@@ -55,8 +55,8 @@ if ( empty( $product ) || ! $product->is_visible() ) {
             do_action( 'woocommerce_shop_loop_item_title' ); ?>
 
             <hr>
-            
-            <?php 
+
+            <?php
             /**
              * woocommerce_after_shop_loop_item_title hook.
              *
@@ -73,9 +73,9 @@ if ( empty( $product ) || ! $product->is_visible() ) {
              */
             do_action( 'woocommerce_after_shop_loop_item' );
             ?>
-            
+
             <div class="clear"></div>
 
         </div>
-    
+
 </li>
